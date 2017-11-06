7ececbfc8f91083b2ab7e6ed3066058879b4c15c
Omer Mujtaba <omer_mujtaba@hotmail.com>
2017-11-07 02:57:33 +0500

    Latest Commit

--- OpenGL/Resources/Shader/core.frag
+++ OpenGL/Resources/Shader/core.frag
@@ -1,9 +1,15 @@
 #version 330 core
-
 in vec3 ourColor;
+in vec2 TexCoord;
 
 out vec4 color;
 
-void main(){
-    color = vec4 (ourColor, 1.0f );
+// Texture samplers
+uniform sampler2D ourTexture1;
+
+void main()
+{
+    // Linearly interpolate between both textures (second texture is only slightly combined)
+    color = texture(ourTexture1, TexCoord);
 }
+
--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -7,10 +7,11 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+// Other Libs
+#include "SOIL2/SOIL2.h"
+
 // Other includes
 #include "Shader.h"
-
-#include "SOIL2/SOIL2.h"
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -57,86 +58,75 @@ int main( )
     // Define the viewport dimensions
     glViewport( 0, 0, screenWidth, screenHeight );
     
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    
+    // enable alpha support
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
     
     // Build and compile our shader program
-    Shader ourShader( "resources/shader/core.vs", "resources/shader/core.frag" );
+    Shader ourShader( "Resources/Shader/core.vs", "Resources/Shader/core.frag" );
     
     // Set up vertex data (and buffer(s)) and attribute pointers
-    
-//    GLfloat vertices[] =
-//    {
-//        // Positions         // Colors
-//        0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
-//        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // Bottom Left
-//        0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top
-//    };
-
-    
     GLfloat vertices[] =
     {
-        // Positions         // Colors              //Textures coordinates
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,      1.0f, 1.0f,            // Top Right
-         0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,      1.0f, 0.0f,            // Bottom Right
-        -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,      0.0f, 0.0f,            // Top Left
-        -0.5f, 0.5f, 0.0f,    1.0f, 0.0f, 1.0f,      0.0f, 1.0f             // Bottom Left
-
+        // Positions          // Colors           // Texture Coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
     };
-
-    GLuint indices[] = {
-        0,1,3,      //First Triangle
-        1,2,3       //Second Triangle
+    GLuint indices[] =
+    {  // Note that we start from 0!
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
     };
-    
     GLuint VBO, VAO, EBO;
     glGenVertexArrays( 1, &VAO );
     glGenBuffers( 1, &VBO );
-    glGenBuffers(1, &EBO);
+    glGenBuffers( 1, &EBO );
     
-    
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
     glBindVertexArray( VAO );
     
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
-    glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
     
     glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
-    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW );
-    
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
     
     // Position attribute
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * ) 0 );
-    glEnableVertexAttribArray( 0 );
+    glEnableVertexAttribArray(0);
     // Color attribute
     glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )( 3 * sizeof( GLfloat ) ) );
-    glEnableVertexAttribArray( 1 );
-    // Texture coordinate attribute
+    glEnableVertexAttribArray(1);
+    // Texture Coordinate attribute
     glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof( GLfloat ), ( GLvoid * )( 6 * sizeof( GLfloat ) ) );
     glEnableVertexAttribArray( 2 );
     
     glBindVertexArray( 0 ); // Unbind VAO
     
+    // Load and create a texture
     GLuint texture;
-    int width,height;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
     
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int width, height;
     
-    unsigned char *image = SOIL_load_image("Resources/images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
     
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    SOIL_free_image_data(image);
-    
-    glBindTexture(GL_TEXTURE_2D, 0);
-    
+    // ===================
+    // Texture
+    // ===================
+    glGenTextures( 1, &texture );
+    glBindTexture( GL_TEXTURE_2D, texture );
+    // Set our texture parameters
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    // Set texture filtering
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    // Load, create texture and generate mipmaps
+    unsigned char *image = SOIL_load_image( "Resources/images/image2.png", &width, &height, 0, SOIL_LOAD_RGBA );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
+    glGenerateMipmap( GL_TEXTURE_2D );
+    SOIL_free_image_data( image );
+    glBindTexture( GL_TEXTURE_2D, 0 );
     
     // Game loop
     while ( !glfwWindowShouldClose( window ) )
@@ -151,14 +141,14 @@ int main( )
         
         // Draw the triangle
         ourShader.Use( );
+        glActiveTexture( GL_TEXTURE0 );
+        glBindTexture( GL_TEXTURE_2D, texture );
+        glUniform1i( glGetUniformLocation( ourShader.Program, "ourTexture" ), 0 );
         
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glUniform1i(glGetUniformLocation(ourShader.Program,"ourTexture"),0);
-        
+        // Draw container
         glBindVertexArray( VAO );
-        glDrawArrays( GL_TRIANGLES, 0, 3 );
-        glBindVertexArray(0);
+        glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+        glBindVertexArray( 0 );
         
         // Swap the screen buffers
         glfwSwapBuffers( window );
@@ -167,11 +157,14 @@ int main( )
     // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays( 1, &VAO );
     glDeleteBuffers( 1, &VBO );
-    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers( 1, &EBO );
     
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
     
     return EXIT_SUCCESS;
 }
+
+
+
 
