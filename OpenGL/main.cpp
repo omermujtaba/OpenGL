8859a66a404801c96d0dde66b30836f2aa57a886
Omer Mujtaba <omer_mujtaba@hotmail.com>
2017-11-06 20:01:07 +0500

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Omer Mujtaba on 06/11/2017.
+//  Copyright © 2017 Omer Mujtaba. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
