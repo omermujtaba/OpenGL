4a742262442887c01df0ffeb782e57f27ecb0512
Omer Mujtaba <omer_mujtaba@hotmail.com>
2017-11-06 22:27:55 +0500

    Updated File Structure

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,107 +1,119 @@
 #include <iostream>
 
-//GLEW = GL Extension Wrangler
-
+// GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
 
-//GLFW
+// GLFW
 #include <GLFW/glfw3.h>
 
-//Shader
+// Other includes
 #include "Shader.h"
 
-// Window Deimensions
-const GLint HEIGHT=800, WIDTH = 600;
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
 
-int main(){
+// The MAIN function, from here we start the application and run the game loop
+int main( )
+{
+    // Init GLFW
+    glfwInit( );
     
-    // Initialize GLFW
-    glfwInit();
+    // Set all the required options for GLFW
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
     
-    // Set all required options for GLFW
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
-    //Create GLFWindow for all GLFW functions
-    GLFWwindow *window = glfwCreateWindow(WIDTH,HEIGHT,"Learn GL", nullptr,nullptr);
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr );
     
-    int screenWidth,screenHeight;
-    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
     
-    // Return if GLFW window failed to create
-    if(nullptr == window){
-        std:: cout << "Failed to create GLFW Window" << std::endl;
-        glfwTerminate();
-        return  -1;
+    if ( nullptr == window )
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate( );
+        
+        return EXIT_FAILURE;
     }
     
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent( window );
+    
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
-    
-    // Iniitalize GLEW
-    if(GLEW_OK != glewInit()){
-        std:: cout << "Failed to init GLEW" << std::endl;
-        return -1;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if ( GLEW_OK != glewInit( ) )
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return EXIT_FAILURE;
     }
     
-    glViewport(0, 0, screenWidth, screenHeight);
+    // Define the viewport dimensions
+    glViewport( 0, 0, screenWidth, screenHeight );
     
+    // Build and compile our shader program
+    Shader ourShader( "core.vs", "core.frag" );
     
-    
+    // Set up vertex data (and buffer(s)) and attribute pointers
     GLfloat vertices[] =
     {
-        //position          //color
-        -0.5f, -0.5, 0.0f,  1.0f, 0.0f,0.0f,    //bottom left
-        0.5f, -0.5, 0.0f,   0.0f, 1.0f,0.0f,    //bottom right
-        0.0f, 0.5, 0.0f,    0.0f, 0.0f,1.0f    //top middle
+        // Positions         // Colors
+        0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
+        0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top
     };
     
     GLuint VBO, VAO;
-    glGenVertexArrays(  1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenVertexArrays( 1, &VAO );
+    glGenBuffers( 1, &VBO );
     
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray( VAO );
     
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer( GL_ARRAY_BUFFER, VBO );
+    glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
     
-    glVertexAttribPointer(0,3, GL_FLOAT, GL_TRUE,3 * sizeof(GLfloat), (GLvoid *) 0);
-    glEnableVertexAttribArray(0);
+    // Position attribute
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( GLvoid * ) 0 );
+    glEnableVertexAttribArray( 0 );
+    // Color attribute
+    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( GLvoid * )( 3 * sizeof( GLfloat ) ) );
+    glEnableVertexAttribArray( 1 );
     
-    glBindBuffer(GL_ARRAY_BUFFER,0);
-    glBindVertexArray(0);
+    glBindVertexArray( 0 ); // Unbind VAO
     
-    
-    
-    //Game Loop
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    // Game loop
+    while ( !glfwWindowShouldClose( window ) )
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents( );
         
-        glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        // Render
+        // Clear the colorbuffer
+        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+        glClear( GL_COLOR_BUFFER_BIT );
         
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        // Draw the triangle
+        ourShader.Use( );
+        glBindVertexArray( VAO );
+        glDrawArrays( GL_TRIANGLES, 0, 3 );
         glBindVertexArray(0);
         
-        glfwSwapBuffers(window);
+        // Swap the screen buffers
+        glfwSwapBuffers( window );
     }
     
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteBuffers( 1, &VBO );
     
-    glfwTerminate();
-    return 0;
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate( );
     
-    
+    return EXIT_SUCCESS;
 }
-
-
 
